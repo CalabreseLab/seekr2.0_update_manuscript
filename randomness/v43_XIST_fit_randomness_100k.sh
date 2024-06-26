2f3a2c1a97825ff3898891688cc74d468a930569
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2024-06-26 16:54:42 -0400

    Update v43_XIST_fit_randomness_100k.sh

--- randomness/v43_XIST_fit_randomness_100k.sh
+++ randomness/v43_XIST_fit_randomness_100k.sh
@@ -11,49 +11,42 @@ seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o 
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_1.csv -o v43vXIST_100k_1
 
-seekr_adj_pval v43vXIST_100k_1.csv fdr_bh -o v43vXIST_100k_bh_1
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_2
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_2.csv -o v43vXIST_100k_2
 
-seekr_adj_pval v43vXIST_100k_2.csv fdr_bh -o v43vXIST_100k_bh_2
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_3
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_3.csv -o v43vXIST_100k_3
 
-seekr_adj_pval v43vXIST_100k_3.csv fdr_bh -o v43vXIST_100k_bh_3
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_4
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_4.csv -o v43vXIST_100k_4
 
-seekr_adj_pval v43vXIST_100k_4.csv fdr_bh -o v43vXIST_100k_bh_4
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_5
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_5.csv -o v43vXIST_100k_5
 
-seekr_adj_pval v43vXIST_100k_5.csv fdr_bh -o v43vXIST_100k_bh_5
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_6
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_6.csv -o v43vXIST_100k_6
 
-seekr_adj_pval v43vXIST_100k_6.csv fdr_bh -o v43vXIST_100k_bh_6
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_7
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_7.csv -o v43vXIST_100k_7
 
-seekr_adj_pval v43vXIST_100k_7.csv fdr_bh -o v43vXIST_100k_bh_7
 
 
 
@@ -61,21 +54,18 @@ seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o 
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_8.csv -o v43vXIST_100k_8
 
-seekr_adj_pval v43vXIST_100k_8.csv fdr_bh -o v43vXIST_100k_bh_8
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_9
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_9.csv -o v43vXIST_100k_9
 
-seekr_adj_pval v43vXIST_100k_9.csv fdr_bh -o v43vXIST_100k_bh_9
 
 
 seekr_find_dist v43_canonical.fa -k 6 -mdl common10 -sbt -sbs 100000 -fm -pb -o v43_100k_10
 
 seekr_find_pval v43_canonical.fa XIST.fa mean_6mers.npy std_6mers.npy 6 v43_100k_10.csv -o v43vXIST_100k_10
 
-seekr_adj_pval v43vXIST_100k_10.csv fdr_bh -o v43vXIST_100k_bh_10
 
 
 
